c0d545f340aef4dc63b3778926e4d78affc40156
unknown <G00463317@atu.ie>
2026-04-12 13:34:55 +0000

    Added movies list and search bar

---
+++ src/app/movieServices/movie.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  private apiUrl = 'https://www.omdbapi.com/?apikey=2ee2ed84';
+
+  constructor(private http: HttpClient) {}
+
+  GetMovieData(query: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}&s=${query}`);
+  }
+}
--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,} from '@ionic/angular/standalone';
+import { MovieService } from '../movieServices/movie';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.page.html',
   styleUrls: ['./movies.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, CommonModule, FormsModule]
 })
+
+
 export class MoviesPage{
-
+  
+  query = '';
   movies:any[]=[];
-  constructor(private service:MovieService) {}
 
-  ionViewWillEnter(){
-    this.service.GetMovieData().subscribe(         // subscribes to event to get movie info
-      (data)=>{
-        this.movies = data.Search;                 
-        console.log(this.movies);                  
-      }
-    );
+  constructor(private movieService: MovieService) {}
 
-}
+    ionViewWillEnter() {
+      this.movieService.GetMovieData('war').subscribe((data) => {
+        this.movies = data.Search || [];
+      });
+    }
+
+search(event?: any) {
+  const value = event?.detail?.value ?? this.query;
+  if (!value.trim()) return;
+  this.movieService.GetMovieData(value).subscribe((data) => {
+    this.movies = data.Search || [];
+  });
+    }
+
+  }
+
